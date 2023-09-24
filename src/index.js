8e8c642b3f0f408ba9ccb126165bd29dfa5e8a86
mohanraj <mraj8867@gmail.com>
2023-09-24 21:54:14 +0530

    Update projects, checklist items, and To-Do section, add event listener, and update CSS grid layout and cursor styles
    Commit Message: This commit updates the style, functionality, HTML
    structure, and content of projects, checklist items, and the To-Do
    section, adds an event listener to toggle the display of project
    items, and updates the CSS grid layout and cursor styles.
    
    Commit Description:
    - Updates the style of the projects and checklist items
    - Adds functionality to toggle the display of project items when
    clicked
    - Adds a checklist icon image in the header
    - Updates the HTML structure and content
    - Adds a new section for To-Do list
    - Adds a sidebar with project items
    - Adds a project content section
    - Removes unnecessary div elements and previous content
    - Changes to the To-Do section by adding a new paragraph
    - Restructures the project content by adding a sidebar with project
    items
    - Adds an event listener to the 'projects' element
    - Toggles the display of the 'project_item' element based on its
    current display property
    - Changes to the CSS file
    - Updates the grid layout for the specified classes
    - Changes the cursor style for the projects and projects-item classes
    - Removes flexbox properties and replaces them with grid properties
    for better layout control

--- src/index.js
+++ src/index.js
@@ -5,3 +5,23 @@ const checkListImg = document.createElement('img');
 checkListImg.setAttribute('src', checkList)
 const header = document.querySelector('header');
 header.appendChild(checkListImg);
+
+const projects = document.querySelector('.projects');
+const project_item = document.querySelector('.projects-item');
+projects.addEventListener('click',() => {
+    
+    const projectItemDisplay = window.getComputedStyle(project_item).getPropertyValue('display');
+    
+    switch (projectItemDisplay) {
+        case 'none':
+            project_item.style.display = 'block';
+            break;
+        case 'block':
+            project_item.style.display = 'none';
+            break;
+    
+        default:
+            break;
+    }
+} );
+
