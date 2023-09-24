272ecaac43e116d619eebe26783046f6d43edcd7
mohanraj <mraj8867@gmail.com>
2023-09-24 20:06:26 +0530

    Update style.css and index.js, add checklist image and new styling
    ommit Message:	This commit updates the style.css and index.js files,
     removes unnecessary code from the To-Do list and styling, adds a
     checklist image to the header, and makes updates to the element
    styles by including a border, width, and height.
    Commit Description:
    - Updates style.css and index.js files
    - Adds new CSS styles
    - Imports an SVG image
    - Appends SVG image to header element
    - Adds new item to To-Do list
    - Removes unnecessary code causing issues
    - Adds new styling for To-Do element
    - Removes unnecessary code no longer needed
    - Adds checklist image to header
    - Imports necessary files
    - Appends image element to header
    - Adds border to element
    - Sets width to 100vw
    - Sets height to 100vh

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+import checkList from './checklist_FILL0_wght400_GRAD0_opsz24.svg'
+
+const checkListImg = document.createElement('img');
+checkListImg.setAttribute('src', checkList)
+const header = document.querySelector('header');
+header.appendChild(checkListImg);
